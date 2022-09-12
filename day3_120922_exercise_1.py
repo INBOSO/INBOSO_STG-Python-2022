bf3537e27f2114b341f90e83a4b7ef5270087ea9
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-12 20:51:52 +0300

    .

--- day3_120922_exercise_1.py
+++ day3_120922_exercise_1.py
@@ -1,7 +1,16 @@
 # 1. Health check
 
 # Ask user for their temperature.
+a = int(input("What is your temperature "));
 
+text= "possible fever";
+if a < 35:
+    text= "not too cold";
+
+elif a <= 37:   
+    text= "all right";
+
+print(f"Temperature:  {a} : {text}")
 # If the user enters below 35, then output "not too cold"
 
 # If 35 to 37 (inclusive), output "all right"
