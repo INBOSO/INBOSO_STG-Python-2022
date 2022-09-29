d623fd3aaab215a38e3854b6ffa545968a945e02
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-29 13:36:16 +0300

    upd

--- Day3_120922/day3_logic.py
+++ Day3_120922/day3_logic.py
@@ -4,49 +4,49 @@ print(is_sunny)
 print("let the toggling begin with not")
 is_sunny = not is_sunny # Toggle pattern we reverse the logic
 print(is_sunny)
-# is_sunny = not is_sunny # we reverse the logic
-# print(is_sunny)
-# is_sunny = not is_sunny # we reverse the logic
-# print(is_sunny)
-# is_overcast = not is_sunny #  but if it is not sunny then it is overcast
-# print(is_overcast, is_sunny)
-# print(not True, not False) # negation Python uses not, some other language use !
+is_sunny = not is_sunny # we reverse the logic
+print(is_sunny)
+is_sunny = not is_sunny # we reverse the logic
+print(is_sunny)
+is_overcast = not is_sunny #  but if it is not sunny then it is overcast
+print(is_overcast, is_sunny)
+print(not True, not False) # negation Python uses not, some other language use !
 # # # # #
 # # # # # # logical conjunction and in Python we use and (not &&)
-# print("Testing and logic, logical conjunction")
-# print(True and True) # only True here
-# print(True and False)  # only False here
-# print(False and True) # only False here
-# print(False and False) # last 3 are False
+print("Testing and logic, logical conjunction")
+print(True and True) # only True here
+print(True and False)  # only False here
+print(False and True) # only False here
+print(False and False) # last 3 are False
 # # # # #
 # # # # # we can have conjuctions on multiple statements
-# print(is_sunny) # False
-# print(2*2 == 4 and is_sunny and 5 > 1) # False because is_sunny is False
+print(is_sunny) # False
+print(2*2 == 4 and is_sunny and 5 > 1) # False because is_sunny is False
 # # ## lazy evaluation in the above first false and rest is not evaluated
 # # # # # # above is False since in a chain one False statement will ruin everything
-# a = 0
+a = 0
 # # print(551/a)    # will throw an error
-# print( a != 0 and 551 / a) # so 10 / a will not run, so no error will be thrown
+print( a != 0 and 551 / a) # so 10 / a will not run, so no error will be thrown
 # # #
 # # is_sunny = not is_sunny # we reverse the logic
 # # print(2*2 == 4 and is_sunny and 5 > 1 and 5*5 > 10)
 # # # # #
 # # # # # logical disjunction with or (Python uses actual or not || like other languages)
 # # so in or you only need one True to get True
-# print(True or True)
-# print(True or False)
-# print(False or True) # all 3 of the above ar True
-# print(False or False) # only one which is False
+print(True or True)
+print(True or False)
+print(False or True) # all 3 of the above ar True
+print(False or False) # only one which is False
 # # #
-# are_pigs_flying = False
-# is_devil_skating = False
-# is_winter = True
+are_pigs_flying = False
+is_devil_skating = False
+is_winter = True
 # # so one of the above is True so we get True
-# print(are_pigs_flying or is_devil_skating or is_winter or 2 * 2 == 5) # so True...we just need one True statement
-# is_a_good_day = are_pigs_flying or is_devil_skating or is_winter or 2 * 2 == 5
-# print(is_a_good_day)
-# is_winter = False
-# print(are_pigs_flying or is_devil_skating or is_winter or 2 * 2 == 5)  # when everything is False then whole statement is False
+print(are_pigs_flying or is_devil_skating or is_winter or 2 * 2 == 5) # so True...we just need one True statement
+is_a_good_day = are_pigs_flying or is_devil_skating or is_winter or 2 * 2 == 5
+print(is_a_good_day)
+is_winter = False
+print(are_pigs_flying or is_devil_skating or is_winter or 2 * 2 == 5)  # when everything is False then whole statement is False
 # # #
 # # # # #
 # # Secondary logic operators you implement them  yourself
