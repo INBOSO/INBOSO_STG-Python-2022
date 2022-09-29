7ff51ac87babf71f599668ef612c27c504443f41
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-29 15:29:23 +0300

    upd

--- Day3_120922/day3_class_exer2.py
+++ Day3_120922/day3_class_exer2.py
@@ -1,22 +1,20 @@
-m_salary_amount = input("What is your monthly salary amount? ")
-years_worked = input("How long are you with company? (in years) ")
-bonus = m_salary_amount * 0,15
-# if years_worked >=2:
-#     print ("Your bonus 15% {m_salary_amount} is: ")
+m_salary_amount = int(input("What is your monthly salary amount? "))
+years_worked = float(input("How long are you with company? (in years) "))
+bonus = m_salary_amount * 0.15 * years_worked
 
-print(m_salary_amount)
+if years_worked >= 2:
+    print (f"Your bonus 15% from {m_salary_amount} is: {bonus}")
+else:
+    print ("No bonus (0)")
+
 
 
 # 2. Xmas Bonus
 
 # The company has promised a Christmas bonus in the amount of 15% of the monthly salary for EVERY year of 
 # service over 2 years.
-
 # Task. Ask the user for the amount of the monthly salary and the number of years worked.
-
 # Calculate the bonus.
-
 # Example1: 5 years of experience, 1000 Euro salary, the bonus will be 450 Euro.
-
 # Example2: 1.5 years of experience, 1500 Euro salary, no bonus(0)
 
