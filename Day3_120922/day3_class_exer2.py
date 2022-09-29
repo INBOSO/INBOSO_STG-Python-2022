b171d8a624bab99643902632e93f592029778ad0
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-29 16:13:46 +0300

    upd

--- Day3_120922/day3_class_exer2.py
+++ Day3_120922/day3_class_exer2.py
@@ -1,21 +1,22 @@
 
-#1
-m_salary_amount = int(input("What is your monthly salary amount? "))
-years_worked = float(input("How long are you with company? (in years) "))
-bonus = m_salary_amount * 0.15 * (years_worked-2)
+# #1
+# m_salary_amount = int(input("What is your monthly salary amount? "))
+# years_worked = float(input("How long are you with company? (in years) "))
+# bonus = m_salary_amount * 0.15 * (years_worked-2)
 
-if years_worked >= 2:
-    print (f"Your bonus 15% from {m_salary_amount} is: {bonus}")
-else:
-    print ("No bonus (0)")
+# if years_worked >= 2:
+#     print (f"Your bonus 15% from {m_salary_amount} is: {bonus}")
+# else:
+#     print ("No bonus (0)")
 
 #2
 years_worked = float(input("How long are you with company? (in years) "))
 
 if years_worked > 2:
-    m_salary_amount = int(input("What is your monthly salary amount? "))
-    bonus = m_salary_amount * 0.15 * (years_worked-2)
-    print (f"Your bonus 15% from {m_salary_amount} is: {bonus}")
+    m_salary_amount = float(input("What is your monthly salary amount? "))
+    bonus_rate = 0.15
+    bonus_amount = round(m_salary_amount * bonus_rate * (years_worked-2),2)
+    print (f"Your bonus 15% from {m_salary_amount} is: {bonus_amount}")
 
 else:
     print ("No bonus (0)")
