9fdf7e668be2a61ef2eea605abf59baea90c8b83
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-29 16:07:37 +0300

    upd

--- Day3_120922/day3_class_exer1.py
+++ Day3_120922/day3_class_exer1.py
@@ -1,6 +1,4 @@
-# 1. Health check
-
-# Ask user for their temperature.
+# #1
 a = float(input("What is your temperature "))
 
 text= "possible fever"
@@ -11,10 +9,29 @@ elif a <= 37:
     text= "all right"
 
 print(f"Temperature:  {a} : {text}")
+
+#2
+temp = float(input("What is your temperature? "))
+if temp < 35:
+    print("not too cold", temp)
+elif temp <= 37:
+    print("all right", temp)
+else:
+    print("possible fever")
+#3
+normal_temp = 37
+cold_temp = 35
+
+temp = float(input("What is your temperature? "))
+if temp < cold_temp:
+    print("not too cold", temp)
+elif temp <= normal_temp:
+    print("all right", temp)
+else:
+    print("possible fever")
+# 1. Health check
+# Ask user for their temperature.
 # If the user enters below 35, then output "not too cold"
-
 # If 35 to 37 (inclusive), output "all right"
-
 # If the temperature  over 37, then output  "possible fever
-
 # remember about type conversion if needed
