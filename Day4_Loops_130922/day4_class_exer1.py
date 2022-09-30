4ddc7992efae0ee4de9305df3b477dc2b189ffcd
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-30 16:43:47 +0300

    upd

--- Day4_Loops_130922/day4_class_exer1.py
+++ Day4_Loops_130922/day4_class_exer1.py
@@ -9,3 +9,19 @@
 #  Note: such a task became popular as the first task to ask to determine 
 #  whether a person knows about programming at all smile
 
+fizzbuzz_start = int(input("Enter a number for FizzBuzz starts? "))
+fizzbuzz_ends = int(input("What will be the end of the FizzBuzz? "))
+fizz = int(input("Enter number for Fizz "))
+buzz = int((input("Enter the number for Buzz ")))
+
+for fizzbuzz in range(fizzbuzz_start,fizzbuzz_ends):
+    if fizzbuzz % fizz == 0 and fizzbuzz % buzz == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % fizz == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % buzz == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
