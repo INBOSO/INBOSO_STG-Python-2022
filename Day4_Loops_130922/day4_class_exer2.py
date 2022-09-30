169eaec77dcab65eae81e46ea33b94b389265e22
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-09-30 16:43:42 +0300

    upd

--- Day4_Loops_130922/day4_class_exer2.py
+++ Day4_Loops_130922/day4_class_exer2.py
@@ -6,5 +6,11 @@
 #   * 
 #  *** 
 # ***** 
-# Note: remember that several symbols can be 
-# printed at once, for example: print ("" * 10 + "*" * 6)
+# Note: remember that several symbols can be # printed at once, for example: print ("" * 10 + "*" * 6)
+
+tree_high = int(input("Please enter theheigh of the tree "))
+star = 1
+
+for i in range (1,tree_high+1):
+    print(" "*(tree_high-i)+ "*" * star)
+    star +=2 
