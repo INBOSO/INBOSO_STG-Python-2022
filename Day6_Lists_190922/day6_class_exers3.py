ef4011e8d691635374fd9b6bf40188dff3b31fd2
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-10-05 19:50:19 +0300

    upd

--- Day6_Lists_190922/day6_class_exers3.py
+++ Day6_Lists_190922/day6_class_exers3.py
@@ -11,3 +11,12 @@
 # notice how each word was reversed separately
 
 # PS Split and join operations could be useful here.
+
+
+sentence = input("Please enter the sentence ")
+word_list = sentence.split()
+print(word_list)
+reversed_list = list(reversed(word_list))
+print(reversed_list)
+new_sentence = " ".join(reversed_list)
+print(new_sentence)
