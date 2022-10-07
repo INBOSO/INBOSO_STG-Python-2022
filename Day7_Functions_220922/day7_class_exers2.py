47e04733ff00a42008816e340ca4f44223aab21c
Inna Borshch <79091711+INBOSO@users.noreply.github.com>
2022-10-07 21:08:54 +0300

    upd

---
+++ Day7_Functions_220922/day7_class_exers2.py
@@ -0,0 +1,17 @@
+# 2. Palindrome
+
+
+# Write the function is_palindrome(text)
+
+# which returns a bool True or False depending on whether the word or sentence is read equally from both sides.
+
+# PS You can start with a one-word solution from the beginning, but the full solution will ignore whitespace and uppercase and lowercase letters.
+
+
+# is_palindrome ("Alus ari i ra    sula") -> True
+
+# is_palindrome("ABa") -> True
+
+# is_palindrome("nava") -> False
+# Python has type hints, which are not mandatory but can be used to help the developer
+# type hints are like United Nations letters, they are not mandatory but they are useful
